7fff8d3fe3940b19b81453e5346783cd5f389231
Ramu Kotharu <ramanji.sql@gmail.com>
2023-10-19 07:45:11 +0530

    Bonus 3rd Question SignOff
    Bonus 3rd Question Sign Off

--- Python-Code/Bonus-Questions/BonusThird.py
+++ Python-Code/Bonus-Questions/BonusThird.py
@@ -1,11 +1,15 @@
 import mmap
-
 from pyspark.sql import SparkSession
 import logging
 import configparser
 import csv
+from collections import defaultdict
+from operator import itemgetter
+from heapq import nlargest
 
 logging.basicConfig(level=logging.INFO)
+
+""" Implemented bonus third question (hard): using Question: 2"""
 
 
 class Solution:
@@ -16,59 +20,52 @@ class Solution:
         return config
 
     @staticmethod
-    def get_top_airports(rec):
-        dict = {}
+    def top_airports(airport_pass, N, airport_city):
+        result = {}
+        for k, v in airport_pass.items():
+            result[k] = sum(v)
+        final_dict = dict(sorted(result.items(), key=lambda x: x[1], reverse=True)[:int(N)])
+        for key in final_dict.keys():
+            print("Airport-->",key, "  City--->", airport_city[key], "Count of Passengers-->",  final_dict[key])
+
+    @staticmethod
+    def get_airport_pass(rec, config):
+
+        airport_pass = {}
+        airport_city = {}
 
         for x in rec:
             try:
-                if(len(x)>500):
-                  key = x.split("^")[12].strip()
-                  value = int(x.split("^")[-4])
-                else:
-                    key = x.split("^")[12].strip()
-                    value = int(x.split("^")[-3])
-
-                if dict.__contains__(key):
-                    dict[key][len(dict.keys())] = value
-                else:
-                    dict[key] = {}
-                    dict[key][0] = value
+                if int(x.split("^")[-3]) == 2013:
+                    lst = x.split("^")
+                    airport_pass.setdefault(lst[12].strip(), []).append(int(lst[-4]))
+                    airport_city[lst[12].strip()] = lst[13].strip()
             except:
-                print("invalid record--->", x)
-
-
-        print(dict)
+                logging.exception('{}:{}'.format("exception due to the record",x))
+        Solution.top_airports(airport_pass, config.get("input", "top"), airport_city)
 
     def process_bookings(self, config):
         logging.info("Reading the bookings file")
-        rec = set()
+        valid_recs = set()
+        corrupted_recs = set()
         with open(config.get("input", "booking_file")) as file_obj:
+            logging.info("reading the header to count of columns in the file")
+            reader = csv.reader(file_obj, delimiter="^")
+            ncol = len(next(reader))
+            logging.info('{}:{}'.format("Number of columns", ncol))
+            logging.info("Reading the csv file in chunks, to improve the performance and memory efficient")
             reader_obj = csv.reader(file_obj, int(config.get("input", "chunk_size")))
             next(reader_obj, None)
             for row in reader_obj:
                 for x in row:
-                    if (len(x) > 200):
-                        rec.add(x)
-
-        #logging.info('{}:{}'.format("distinct booking count", len(rec)))
+                    if len(x.strip().split("^")) == ncol:
+                        valid_recs.add(x)
+                    else:
+                        corrupted_recs.add(x)
         file_obj.close()
-
-        Solution.get_top_airports(list(rec))
-
-    def read_search_file(self, config):
-        logging.info("Reading the searches file")
-        rec = set()
-        with open(config.get("input", "search_file")) as file_obj:
-            reader_obj = csv.reader(file_obj, int(config.get("input", "chunk_size")))
-            next(reader_obj, None)
-            for row in reader_obj:
-                for x in row:
-                    rec.add(x)
-        logging.info('{}:{}'.format("distinct search count", len(rec)))
-        file_obj.close()
+        Solution.get_airport_pass(list(valid_recs), config)
 
 
 amadeus = Solution()
 config = amadeus.config_properties()
 amadeus.process_bookings(config)
-# amadeus.read_search_file(config)
