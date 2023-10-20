f4b91889729f754a8594971aa2be5f1069e6c7a3
Ramu Kotharu <ramanji.sql@gmail.com>
2023-10-20 07:43:34 +0530

    Signing of Bonus-2 Question
    Signing of Bonus-2 Question

--- Python-Code/Bonus-Questions/QuestionTwoRdd.py
+++ Python-Code/Bonus-Questions/QuestionTwoRdd.py
@@ -5,6 +5,7 @@ from operator import add
 
 logging.basicConfig(level=logging.INFO)
 
+"""  Implemented Bonus-2 question with Spark RDD."""
 
 class Solution:
 
@@ -23,10 +24,22 @@ class Solution:
         logging.info("Reading the bookings file")
         bookings_rdd = sc.textFile(config.get("input", "booking_file"))
         header = bookings_rdd.first()
+        ncol = len(header.split("^"))
+        logging.info("{}:{}".format("Number of columns in file-->", ncol ))
         bookings = bookings_rdd.filter(lambda row: row != header).distinct()
-        bookings = bookings.map(lambda row: row.split("^")).map(lambda row: (row[12].strip(), int(row[-4])))
-        countPass = bookings.reduceByKey(add).takeOrdered(10, key=lambda x: -x[1])
-        logging.info('{}:{}'.format("Booking distinct Count", countPass))
+        valid_bookings = bookings.filter(lambda x: (len(x.strip().split("^")) == ncol))
+        logging.info("filter Airport and Passenger count field")
+        air_port_pass = valid_bookings.map(lambda row: row.split("^")) \
+            .map(lambda row: (row[12].strip(), int(row[-4])))
+        logging.info("filter Airport and City fields")
+        air_port_city = valid_bookings.map(lambda row: row.split("^")) \
+            .map(lambda row: (row[12].strip(), row[13].strip())).distinct()
+        logging.info("Counting the passengers by Airport ")
+        pass_count_by_air_port = air_port_pass.reduceByKey(add)
+        final_result = air_port_city.join(pass_count_by_air_port) \
+            .sortBy(lambda x: x[1][1], False)
+        logging.info("The output is in the order of Air port, City and Passenger count")
+        logging.info('{}:{}'.format("Booking distinct Count", final_result.take(int(config.get("input", "top")))))
 
 
 amadeus = Solution()
